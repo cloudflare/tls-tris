5c94a7b9cbc169b1a5e6d7a599efb02293519126
Filippo Valsorda <filippo@cloudflare.com>
2016-06-06 17:24:25 +0100

    Add instructions and code to test against Firefox Nightly

--- _dev/tris-publicserver/server.go
+++ _dev/tris-publicserver/server.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
 
 	"rsc.io/letsencrypt"
@@ -14,12 +16,12 @@ import (
 func main() {
 	var (
 		hostname = flag.String("h", "", "the hostname to obtain a certificate for")
-		target   = flag.String("t", "", "the target hostname to reverse proxy to")
+		target   = flag.String("t", "", "the target to reverse proxy to")
 		email    = flag.String("e", "", "the email to register to Let's Encrypt")
 	)
 	flag.Parse()
 	if *hostname == "" || *target == "" {
-		fmt.Fprintf(os.Stderr, "usage: %s -h tls13.example.com -t blog.example.com [-e me@example.com]\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s -h tls13.example.com -t https://blog.example.com [-e me@example.com]\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
@@ -47,5 +49,20 @@ func main() {
 	defer l.Close()
 
 	s := &http.Server{}
+	http.Handle("/", NewReverseProxy(*target))
 	log.Println(s.Serve(l))
+}
+
+func NewReverseProxy(target string) *httputil.ReverseProxy {
+	t, err := url.Parse(target)
+	if err != nil {
+		panic(err)
+	}
+	director := func(req *http.Request) {
+		req.URL.Scheme = t.Scheme
+		req.URL.Host = t.Host
+		req.Host = t.Host
+
+	}
+	return &httputil.ReverseProxy{Director: director}
 }
