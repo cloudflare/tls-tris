92676b2dbfe2e6d03b8bc76e6f37d36452b29c92
Filippo Valsorda <filippo@cloudflare.com>
2016-08-27 14:16:21 +0100

    Minor tweaks to the compilation monster

--- _dev/go.sh
+++ _dev/go.sh
@@ -3,9 +3,9 @@ set -e
 
 BASEDIR=$(cd "$(dirname "$0")" && pwd)
 
-make --quiet -C "$BASEDIR" go/bin/go
+make --quiet -C "$BASEDIR" go/bin/go >&2
 
-GO="$BASEDIR/go/bin/go" make --quiet -C "$BASEDIR" GOROOT
+GO="$BASEDIR/go/bin/go" make --quiet -C "$BASEDIR" GOROOT >&2
 export GOROOT="$BASEDIR/GOROOT"
 
 exec $BASEDIR/go/bin/go "$@"
