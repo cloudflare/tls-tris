4d0ce43e04e35651a60a8156a9b97f6ab1077df2
Filippo Valsorda <filippo@cloudflare.com>
2016-09-14 15:32:58 +0100

    _dev: don't wipe go/GOROOT across cross-compiles

--- _dev/go.sh
+++ _dev/go.sh
@@ -3,9 +3,10 @@ set -e
 
 BASEDIR=$(cd "$(dirname "$0")" && pwd)
 
-make --quiet -C "$BASEDIR" go/bin/go >&2
+make --quiet -C "$BASEDIR" go >&2
+GOENV="$(go env GOOS)_$(go env GOARCH)"
 
-GO="$BASEDIR/go/bin/go" make --quiet -C "$BASEDIR" GOROOT >&2
-export GOROOT="$BASEDIR/GOROOT"
+GO="$BASEDIR/go_$GOENV/bin/go" make --quiet -C "$BASEDIR" GOROOT >&2
+export GOROOT="$BASEDIR/GOROOT/$GOENV"
 
-exec $BASEDIR/go/bin/go "$@"
+exec $BASEDIR/go_$GOENV/bin/go "$@"
