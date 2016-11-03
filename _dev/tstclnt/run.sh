75f8b4791cd0617e5a04c638822be436f983cb40
Filippo Valsorda <filippo@cloudflare.com>
2016-11-03 14:13:18 -0700

    tris: import go wrapper and interoperability tests

---
+++ _dev/tstclnt/run.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+IFS=':' read -ra ADDR <<< "$1"
+shift
+HOST="${ADDR[0]}"
+PORT="${ADDR[1]}"
+
+exec /dist/OBJ-PATH/bin/tstclnt -D -V tls1.3:tls1.3 -o -O -h $HOST -p $PORT -v "$@" < /httpreq.txt
