8052dc002f55c2274ca42d9b51f5d531a6c04c6b
Filippo Valsorda <filippo@cloudflare.com>
2016-11-04 17:07:36 -0700

    tris: extend ConnectionInfo

--- 13.go
+++ 13.go
@@ -19,6 +19,7 @@ func (hs *serverHandshakeState) doTLS13Handshake() error {
 	c := hs.c
 
 	hs.c.cipherSuite, hs.hello13.cipherSuite = hs.suite.id, hs.suite.id
+	hs.c.clientHello = hs.clientHello.marshal()
 
 	// When picking the group for the handshake, priority is given to groups
 	// that the client provided a keyShare for, so to avoid a round-trip.
