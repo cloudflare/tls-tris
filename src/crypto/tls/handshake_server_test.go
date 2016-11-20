f572732539efcbc290eba342a7af785b4b314704
Filippo Valsorda <hi@filippo.io>
2016-11-20 10:13:40 -0500

    [dev.tls] crypto/tls: implement TLS 1.3 cipher suites
    I opted for adding a separate TLS13CipherSuites field to the Config
    because library users that did not set Config.MaxVersion are
    supposed to get TLS 1.3 support automatically, like it has been for
    HTTP/2, but having set CipherSuites would effectively disable it.
    
    Updates #9671
    
    Change-Id: I26a2776b68374d6f5ee45629da09f9494fe723ad

--- src/crypto/tls/handshake_server_test.go
+++ src/crypto/tls/handshake_server_test.go
@@ -38,9 +38,24 @@ func (zeroSource) Read(b []byte) (n int, err error) {
 var testConfig *Config
 
 func allCipherSuites() []uint16 {
-	ids := make([]uint16, len(cipherSuites))
-	for i, suite := range cipherSuites {
-		ids[i] = suite.id
+	var ids []uint16
+	for _, suite := range cipherSuites {
+		if suite.flags&suiteTLS13 != 0 {
+			continue
+		}
+		ids = append(ids, suite.id)
+	}
+
+	return ids
+}
+
+func allTLS13CipherSuites() []uint16 {
+	var ids []uint16
+	for _, suite := range cipherSuites {
+		if suite.flags&suiteTLS13 == 0 {
+			continue
+		}
+		ids = append(ids, suite.id)
 	}
 
 	return ids
@@ -55,6 +70,7 @@ func init() {
 		MinVersion:         VersionSSL30,
 		MaxVersion:         VersionTLS12,
 		CipherSuites:       allCipherSuites(),
+		TLS13CipherSuites:  allTLS13CipherSuites(),
 	}
 	testConfig.Certificates[0].Certificate = [][]byte{testRSACertificate}
 	testConfig.Certificates[0].PrivateKey = testRSAPrivateKey
