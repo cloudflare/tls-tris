0828214496e154c955ea255cc62f45f79c5a81a9
Peter Wu <pwu@cloudflare.com>
2017-09-07 17:50:10 +0100

    crypto/tls: replace signatureAndHash by SignatureScheme.
    Consolidate the signature and hash fields (SignatureAndHashAlgorithm in
    TLS 1.2) into a single uint16 (SignatureScheme in TLS 1.3 draft 21).
    This makes it easier to add RSASSA-PSS for TLS 1.2 in the future.
    
    Fields were named like "signatureAlgorithm" rather than
    "signatureScheme" since that name is also used throughout the 1.3 draft.
    
    The only new public symbol is ECDSAWithSHA1, other than that this is an
    internal change with no new functionality.
    
    Change-Id: Iba63d262ab1af895420583ac9e302d9705a7e0f0

--- handshake_client.go
+++ handshake_client.go
@@ -105,7 +105,7 @@ NextCipherSuite:
 	}
 
 	if hello.vers >= VersionTLS12 {
-		hello.signatureAndHashes = supportedSignatureAlgorithms
+		hello.supportedSignatureAlgorithms = supportedSignatureAlgorithms
 	}
 
 	var session *ClientSessionState
@@ -457,12 +457,15 @@ func (hs *clientHandshakeState) doFullHandshake() error {
 			return fmt.Errorf("tls: failed to sign handshake with client certificate: unknown client certificate key type: %T", key)
 		}
 
-		certVerify.signatureAndHash, err = hs.finishedHash.selectClientCertSignatureAlgorithm(certReq.signatureAndHashes, signatureType)
-		if err != nil {
-			c.sendAlert(alertInternalError)
-			return err
+		// SignatureAndHashAlgorithm was introduced in TLS 1.2.
+		if certVerify.hasSignatureAndHash {
+			certVerify.signatureAlgorithm, err = hs.finishedHash.selectClientCertSignatureAlgorithm(certReq.supportedSignatureAlgorithms, signatureType)
+			if err != nil {
+				c.sendAlert(alertInternalError)
+				return err
+			}
 		}
-		digest, hashFunc, err := hs.finishedHash.hashForClientCertificate(certVerify.signatureAndHash, hs.masterSecret)
+		digest, hashFunc, err := hs.finishedHash.hashForClientCertificate(signatureType, certVerify.signatureAlgorithm, hs.masterSecret)
 		if err != nil {
 			c.sendAlert(alertInternalError)
 			return err
@@ -721,10 +724,7 @@ func (hs *clientHandshakeState) getCertificate(certReq *certificateRequestMsg) (
 				signatureSchemes = signatureSchemes[:len(signatureSchemes)-tls11SignatureSchemesNumRSA]
 			}
 		} else {
-			signatureSchemes = make([]SignatureScheme, 0, len(certReq.signatureAndHashes))
-			for _, sah := range certReq.signatureAndHashes {
-				signatureSchemes = append(signatureSchemes, SignatureScheme(sah.hash)<<8+SignatureScheme(sah.signature))
-			}
+			signatureSchemes = certReq.supportedSignatureAlgorithms
 		}
 
 		return c.config.GetClientCertificate(&CertificateRequestInfo{
