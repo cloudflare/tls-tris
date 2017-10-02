CongLeSolutionX/go_community

crypto/tls: clarify distinction between signature and key type
A public key algorithm like RSA can be used in multiple different
signature algorithms (e.g. RSASSA-PKCS1-v1_5 and RSASSA-PSS). Introduce
two special types to prevent confusion.

Change-Id: Ia4a59f63ab7f377d46afc792f53755296762f300

src/crypto/tls/handshake_client.go
@@ -471,12 +471,12 @@ func (hs *clientHandshakeState) doFullHandshake() error {
 			return fmt.Errorf("tls: client certificate private key of type %T does not implement crypto.Signer", chainToSend.PrivateKey)
 		}
 
-		var signatureType uint8
+		var signatureType signatureType
 		switch key.Public().(type) {
 		case *ecdsa.PublicKey:
 			signatureType = signatureECDSA
 		case *rsa.PublicKey:
-			signatureType = signatureRSA
+			signatureType = signatureRSAPKCS1
 		default:
 			c.sendAlert(alertInternalError)
 			return fmt.Errorf("tls: failed to sign handshake with client certificate: unknown client certificate key type: %T", key)

src/crypto/tls/handshake_server.go
@@ -520,7 +520,7 @@ func (hs *serverHandshakeState) doFullHandshake() error {
 
 		// Determine the signature type.
 		var signatureAlgorithm SignatureScheme
-		var sigType uint8
+		var sigType signatureType
 		if certVerify.hasSignatureAndHash {
 			signatureAlgorithm = certVerify.signatureAlgorithm
 			if !isSupportedSignatureAlgorithm(signatureAlgorithm, supportedSignatureAlgorithms) {
@@ -536,7 +536,7 @@ func (hs *serverHandshakeState) doFullHandshake() error {
 			case *ecdsa.PublicKey:
 				sigType = signatureECDSA
 			case *rsa.PublicKey:
-				sigType = signatureRSA
+				sigType = signatureRSAPKCS1
 			}
 		}
 
@@ -562,7 +562,7 @@ func (hs *serverHandshakeState) doFullHandshake() error {
 				err = errors.New("tls: ECDSA verification failure")
 			}
 		case *rsa.PublicKey:
-			if sigType != signatureRSA {
+			if sigType != signatureRSAPKCS1 {
 				err = errors.New("tls: bad signature type for client's RSA certificate")
 				break
 			}

src/crypto/tls/prf.go
@@ -313,7 +313,7 @@ func (h finishedHash) serverSum(masterSecret []byte) []byte {
 
 // selectClientCertSignatureAlgorithm returns a SignatureScheme to sign a
 // client's CertificateVerify with, or an error if none can be found.
-func (h finishedHash) selectClientCertSignatureAlgorithm(serverList []SignatureScheme, sigType uint8) (SignatureScheme, error) {
+func (h finishedHash) selectClientCertSignatureAlgorithm(serverList []SignatureScheme, sigType signatureType) (SignatureScheme, error) {
 	for _, v := range serverList {
 		if signatureFromSignatureScheme(v) == sigType && isSupportedSignatureAlgorithm(v, supportedSignatureAlgorithms) {
 			return v, nil
@@ -324,13 +324,13 @@ func (h finishedHash) selectClientCertSignatureAlgorithm(serverList []SignatureS
 
 // hashForClientCertificate returns a digest, hash function, and TLS 1.2 hash
 // id suitable for signing by a TLS client certificate.
-func (h finishedHash) hashForClientCertificate(sigType uint8, signatureAlgorithm SignatureScheme, masterSecret []byte) ([]byte, crypto.Hash, error) {
+func (h finishedHash) hashForClientCertificate(sigType signatureType, signatureAlgorithm SignatureScheme, masterSecret []byte) ([]byte, crypto.Hash, error) {
 	if (h.version == VersionSSL30 || h.version >= VersionTLS12) && h.buffer == nil {
 		panic("a handshake hash for a client-certificate was requested after discarding the handshake buffer")
 	}
 
 	if h.version == VersionSSL30 {
-		if sigType != signatureRSA {
+		if sigType != signatureRSAPKCS1 {
 			return nil, 0, errors.New("tls: unsupported signature type for client certificate")
 		}
 

cloudflare/tls-tris

handshake_client.go
@@ -446,12 +446,12 @@ func (hs *clientHandshakeState) doFullHandshake() error {
 			return fmt.Errorf("tls: client certificate private key of type %T does not implement crypto.Signer", chainToSend.PrivateKey)
 		}
 
-		var signatureType uint8
+		var signatureType signatureType
 		switch key.Public().(type) {
 		case *ecdsa.PublicKey:
 			signatureType = signatureECDSA
 		case *rsa.PublicKey:
-			signatureType = signatureRSA
+			signatureType = signaturePKCS1v15
 		default:
 			c.sendAlert(alertInternalError)
 			return fmt.Errorf("tls: failed to sign handshake with client certificate: unknown client certificate key type: %T", key)

handshake_server.go
@@ -597,7 +597,7 @@ func (hs *serverHandshakeState) doFullHandshake() error {
 
 		// Determine the signature type.
 		var signatureAlgorithm SignatureScheme
-		var sigType uint8
+		var sigType signatureType
 		if certVerify.hasSignatureAndHash {
 			signatureAlgorithm = certVerify.signatureAlgorithm
 			if !isSupportedSignatureAlgorithm(signatureAlgorithm, supportedSignatureAlgorithms) {
@@ -613,7 +613,7 @@ func (hs *serverHandshakeState) doFullHandshake() error {
 			case *ecdsa.PublicKey:
 				sigType = signatureECDSA
 			case *rsa.PublicKey:
-				sigType = signatureRSA
+				sigType = signaturePKCS1v15
 			}
 		}
 
@@ -639,7 +639,7 @@ func (hs *serverHandshakeState) doFullHandshake() error {
 				err = errors.New("tls: ECDSA verification failure")
 			}
 		case *rsa.PublicKey:
-			if sigType != signatureRSA {
+			if sigType != signaturePKCS1v15 {
 				err = errors.New("tls: bad signature type for client's RSA certificate")
 				break
 			}

prf.go
@@ -313,7 +313,7 @@ func (h finishedHash) serverSum(masterSecret []byte) []byte {
 
 // selectClientCertSignatureAlgorithm returns a SignatureScheme to sign a
 // client's CertificateVerify with, or an error if none can be found.
-func (h finishedHash) selectClientCertSignatureAlgorithm(serverList []SignatureScheme, sigType uint8) (SignatureScheme, error) {
+func (h finishedHash) selectClientCertSignatureAlgorithm(serverList []SignatureScheme, sigType signatureType) (SignatureScheme, error) {
 	for _, v := range serverList {
 		if signatureFromSignatureScheme(v) == sigType && isSupportedSignatureAlgorithm(v, supportedSignatureAlgorithms) {
 			return v, nil
@@ -324,13 +324,13 @@ func (h finishedHash) selectClientCertSignatureAlgorithm(serverList []SignatureS
 
 // hashForClientCertificate returns a digest, hash function, and TLS 1.2 hash
 // id suitable for signing by a TLS client certificate.
-func (h finishedHash) hashForClientCertificate(sigType uint8, signatureAlgorithm SignatureScheme, masterSecret []byte) ([]byte, crypto.Hash, error) {
+func (h finishedHash) hashForClientCertificate(sigType signatureType, signatureAlgorithm SignatureScheme, masterSecret []byte) ([]byte, crypto.Hash, error) {
 	if (h.version == VersionSSL30 || h.version >= VersionTLS12) && h.buffer == nil {
 		panic("a handshake hash for a client-certificate was requested after discarding the handshake buffer")
 	}
 
 	if h.version == VersionSSL30 {
-		if sigType != signatureRSA {
+		if sigType != signaturePKCS1v15 {
 			return nil, 0, errors.New("tls: unsupported signature type for client certificate")
 		}
 