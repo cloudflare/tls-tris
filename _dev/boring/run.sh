8db472667bf6426b18b528f0225fa58ec5e16a6f
Peter Wu <pwu@cloudflare.com>
2017-11-14 15:43:14 +0000

    tris: update tests to track draft 22
    Force boringssl client and server to use draft 22 instead of draft 18.
    Other clients (tstclnt from NSS, picotls) support only draft 22.
    
    Disable mint, it only supports draft 21.

--- _dev/boring/run.sh
+++ _dev/boring/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 /boringssl/build/tool/bssl client -grease -min-version tls1.3 -max-version tls1.3 \
-	-session-out /session -connect "$@" < /httpreq.txt
+	-tls13-variant draft22 -session-out /session -connect "$@" < /httpreq.txt
 exec /boringssl/build/tool/bssl client -grease -min-version tls1.3 -max-version tls1.3 \
-	-session-in /session -connect "$@" < /httpreq.txt
+	-tls13-variant draft22 -session-in /session -connect "$@" < /httpreq.txt
 
--- _dev/boring/server.sh
+++ _dev/boring/server.sh
@@ -6,12 +6,14 @@ set -x
 bssl server \
     -key rsa.pem \
     -min-version tls1.2 -max-version tls1.3 \
+    -tls13-draft22-variant \
     -accept 1443 -loop -www 2>&1 &
 
 # ECDSA
 bssl server \
     -key ecdsa.pem \
     -min-version tls1.2 -max-version tls1.3 \
+    -tls13-draft22-variant \
     -accept 2443 -loop -www 2>&1 &
 
 wait
