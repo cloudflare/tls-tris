d453a0025b1639b13f67ad5c020b7941e7badcd9
Kris <kris@amongbytes.com>
2018-02-16 21:07:31 +0000

    (tests) Test for client authentication on server side

--- _dev/tris-localserver/runner.sh
+++ _dev/tris-localserver/runner.sh
@@ -5,5 +5,6 @@
 ./tris-localserver -b 0.0.0.0:3443 -palg=ecdsa -rtt0=o  2>&1 &  # third port: offer and reject 0-RTT
 ./tris-localserver -b 0.0.0.0:4443 -palg=ecdsa -rtt0=oa 2>&1 &  # fourth port: offer and accept 0-RTT
 ./tris-localserver -b 0.0.0.0:5443 -palg=ecdsa -rtt0=oa -rtt0ack 2>&1 &  # fifth port: offer and accept 0-RTT but confirm
+./tris-localserver -b 0.0.0.0:6443 -palg=rsa   -cliauth 2>&1 &  # sixth port: RSA with required client authentication
 
 wait
