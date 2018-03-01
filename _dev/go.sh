cb443468e8fd63e85918e8d005b0effe51dc4f69
Henry D. Case <kris@amongbytes.com>
2018-03-01 14:51:40 +0000

    Makefile improvement
    Following things where added/changed:
    * Builds correctly tls-tris. Previously go had a problem with
      a symbolic link resulting in not building tls-tris at all. I've
      used `rsync' instead.
    * Can build for selected platform with "ARCH=platform make -f Makefile"
    * Possible to build from any directory. Previously ``cd _dev; make``
      was required, it's possible to ``cd /whever/you/want; make -f /tris/Makefile``

--- _dev/go.sh
+++ _dev/go.sh
@@ -4,7 +4,7 @@ set -e
 BASEDIR=$(cd "$(dirname "$0")" && pwd)
 GOENV="$(go env GOHOSTOS)_$(go env GOHOSTARCH)"
 
-make --quiet -C "$BASEDIR" GOROOT >&2
+BUILD_DIR=${BASEDIR}/GOROOT make -f $BASEDIR/Makefile >&2
 
 export GOROOT="$BASEDIR/GOROOT/$GOENV"
 exec go "$@"
