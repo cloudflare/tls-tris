85f0e52037b98be3ba2dcd5e12c01bbb81d58d81
Henry D. Case <kris@amongbytes.com>
2018-08-07 15:24:28 +0100

    tests: Update tests in order to support draft-28
    * Interoperability: Updates NSS and BoringSSL versions to the
      one supporting draft-28
    * Bogo: Updates revision number to use tests for draft-28
    * FIX: makefile was using test-compat target instead of
      test-interop

--- _dev/boring/run.sh
+++ _dev/boring/run.sh
@@ -2,7 +2,7 @@
 set -e
 
 /boringssl/build/tool/bssl client -grease -min-version tls1.3 -max-version tls1.3 \
-	-tls13-variant draft23 -session-out /session -connect "$@" < /httpreq.txt
+	-tls13-variant draft28 -session-out /session -connect "$@" < /httpreq.txt
 exec /boringssl/build/tool/bssl client -grease -min-version tls1.3 -max-version tls1.3 \
-	-tls13-variant draft23 -session-in /session -connect "$@" < /httpreq.txt
+	-tls13-variant draft28 -session-in /session -connect "$@" < /httpreq.txt
 
